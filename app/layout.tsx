0fcda3c08fc7433be851040943014a9a50df9bb3
Anas Ahmed <152002602+AnasAAhmed@users.noreply.github.com>
2026-01-08 22:03:13 +0500

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Poppins } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import './globals.css'
+import Header from '@/components/shared/Header'
+import Footer from '@/components/shared/Footer'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -26,7 +28,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={poppins.variable}>{children}</body>
+        <body className={poppins.variable}>
+             <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+        </body>
       </html>
     </ClerkProvider>
   )
